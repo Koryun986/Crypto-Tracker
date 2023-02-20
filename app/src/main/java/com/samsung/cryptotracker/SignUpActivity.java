19e41724ea8b73d0b6fa7629f4ad2c9b6a390f4b
Koryun <79407889+Koryun986@users.noreply.github.com>
2023-02-20 21:49:54 +0400

    Sign up Activity

--- app/src/main/java/com/samsung/cryptotracker/SignUpActivity.java
+++ app/src/main/java/com/samsung/cryptotracker/SignUpActivity.java
@@ -1,11 +1,9 @@
 package com.samsung.cryptotracker;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.InputType;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.view.View;
@@ -13,22 +11,21 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     ImageView eye;
     EditText email;
     EditText password;
-    TextView toSignUp;
+    TextView toLogin;
     private Boolean isClosedEye = false;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_up);
         eye = findViewById(R.id.password_eye);
-        email = findViewById(R.id.login_emial_edit_text);
-        password = findViewById(R.id.login_password_edit_text);
-        toSignUp = findViewById(R.id.create_account);
+        email = findViewById(R.id.sign_up_emial_edit_text);
+        password = findViewById(R.id.sign_up_password_edit_text);
+        toLogin = findViewById(R.id.to_login);
 
         eye.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,10 +41,10 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        toSignUp.setOnClickListener(new View.OnClickListener() {
+        toLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
